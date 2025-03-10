b923cfbb2bcc6c5ad9b06f0fb81f41461b62dc8e
muhammadogungbo <ogungbomuhammad2024@gmail.com>
2025-03-10 05:19:18 -0700

    push

--- src/Pages/HeroPage.jsx
+++ src/Pages/HeroPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "../Component/Hero";
 import Navbar from "../Component/ReusableComponent/Navbar";
 import Footer from "../Component/ReusableComponent/Footer";
-import Testimonial from "../Component/Testimonial";
+
 const HeroPage = () => {
   return (
     <>
